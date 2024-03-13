45b8ffe92c7dd75244f403cf8d8ff7d9ca9fa6f6
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-03-13 22:42:20 +0500

    Files added

---
+++ src/Pages/CoopGifts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CoopGifts = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CoopGifts
---
+++ src/Pages/LegalEntities.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LegalEntities = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LegalEntities
