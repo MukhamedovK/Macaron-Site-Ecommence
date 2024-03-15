9bbdb11918220166e5f002f5449c605e66112c3e
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-03-15 23:33:34 +0500

    bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
-import Home from './Pages/Home'
-import Basket from './Pages/Basket'
-import ChooseImg from './Pages/ChooseImg'
-
 import Footer from './Pages/Components/Footer'
-import Delivery from './Pages/Delivery'
-
 function App() {
 
   return (
     <>
-      <Router>
-            <Switch>
-                <Route exact path="/" component={<Home />} />
-                <Route path="/basket" component={<Basket />} />
-                <Route path="/chooseimg" component={<ChooseImg />} />
-                <Route component={NotFound} />
-            </Switch>
-        </Router>
+      <Footer />
     </>
   )
 }
